535b40469daf17f175006be98605cb4b893a7d77
Paweł Kołodziejczak <modwebpro@gmail.com>
2019-01-04 10:44:05 +0100

    Update rot3d.js

--- rot3d.js
+++ rot3d.js
@@ -9,61 +9,33 @@ export class rot3d {
   }
 
   _setVars(id, imageDir, image, fps, from) {
-    var _this = this;
+    let _this = this;
 
     _this._app = document.getElementById(id);
-    if (!_this._app) return false;
+    if (!this._app) return false;
 
-    _this._img = _this._app.querySelector(image) || _this._app.getElementsByTagName('img')[0];
-    if (!_this._img && !isImage(_this._img)) return false;
-
-    // rotate on click
-    /*_this._box = document.querySelectorAll('.prodBox');
-    if (!_this._box.length) return false;*/
-    //
+    _this._img = this._app.querySelector(image) || this._app.getElementsByTagName('img')[0];
+    if (!this._img && !isImage(this._img)) return false;
 
     this._ev = {};
     _this._move = 0;
 
-    _this._fps = fps || parseInt(_this._app.getAttribute('data-fps'));
+    _this._fps = fps || parseInt(this._app.getAttribute('data-fps'));
 
     _this._startFrom = from;
     _this._prev = from;
     _this._cur = from;
 
-    _this._dir = imageDir || _this._app.getAttribute('data-dir');
+    _this._dir = imageDir || this._app.getAttribute('data-dir');
     _this._imgArr = [];
 
     return true;
   }
 
   _setEvents(cursor) {
-    //this._changeAnim();
     this._app.style.cursor = cursor;
     this._setRenders();
   }
-
-  /*_changeAnim() {
-      var _this = this;
-
-      this._box._fn = this._box._fn || {};
-      this._box._fn.click = function () {
-
-        each(_this._box, (key, val) => {
-          val.classList.remove('active');
-          this.classList.add('active');
-        });
-
-        _this._ev.destroy();
-        _this._dir = this.getAttribute('data-dir');
-        _this._fps = parseInt(this.getAttribute('data-fps')) || _this._fps;
-        _this._setRenders();
-      };
-
-      each(this._box, (key, val) => {
-        val.addEventListener('click', this._box._fn.click);
-      });
-    }*/
 
   _setRenders() {
     this._img.src = `${this._dir}${this._cur}.png`;
